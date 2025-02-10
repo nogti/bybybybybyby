bd7dc639d77bbb76cc40cbb2de231e3d8924d6d5
Nogti.lua <abagur32@gmail.com>
2025-02-10 18:18:32 +0500

    Add files via upload

---
+++ chams red arms.lua
@@ -0,0 +1,17 @@
+﻿local function changeMaterialToForceField(object)
+    if object:IsA("BasePart") then
+        object.Material = Enum.Material.ForceField
+        object.Color = Color3.fromRGB(255, 0, 0) 
+    end
+end 
+
+local function onChildAdded(child)
+    changeMaterialToForceField(child)
+end
+
+local function onDescendantAdded(descendant)
+    changeMaterialToForceField(descendant)
+end
+
+workspace.CurrentCamera.ChildAdded:Connect(onChildAdded)
+workspace.CurrentCamera.DescendantAdded:Connect(onDescendantAdded)
