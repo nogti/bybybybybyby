5aa47cc52a47e5694cbda25d7c79353c34c370ed
Nogti.lua <abagur32@gmail.com>
2025-02-10 18:18:43 +0500

    Add files via upload

---
+++ 3 litso.lua
@@ -0,0 +1,14 @@
+﻿local player = game.Players.LocalPlayer
+
+local function ensureThirdPerson()
+    if player.CameraMode == Enum.CameraMode.LockFirstPerson then
+        player.CameraMode = Enum.CameraMode.Classic
+    end
+end
+
+player:GetPropertyChangedSignal("CameraMode"):Connect(function()
+    ensureThirdPerson()
+end)
+
+
+ensureThirdPerson()
